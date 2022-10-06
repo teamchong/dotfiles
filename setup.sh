95d055597db44f17051768aee9b7cacf904c9d9c
Steven <25894545+teamchong@users.noreply.github.com>
2022-10-06 09:06:01 +0800

    feat: install GitHub CLI

--- setup.sh
+++ setup.sh
@@ -28,3 +28,10 @@ cat << EOF > ~/.gitignore_global
 .gitpod.yml
 .gitignore_global
 EOF
+# install GitHub CLI
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
